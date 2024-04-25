a84568d1a71576e2da55620fee086e52a0d42721
ivanranking <145848664+ivanranking@users.noreply.github.com>
2024-04-25 10:13:08 +0300

    Add files via upload

---
+++ #a simple basic calculator.py
@@ -0,0 +1,21 @@
+#a simple basic calculator
+print('1= addition')
+
+print("2= multiplication")
+print("3= subtraction")
+print("4 = division")
+choice=input("enter the choice :")
+num1=input("enter num1 :")
+num2=input("enter num2 :")
+if choice == 1:
+    print('num1', "+" , "num2" "=" 'num1 + num2')
+    
+elif choice== "2":
+     print("num1", "*" , "num2" "=" "num1 * num2")
+elif choice== "3":
+      print("num1" , "-", "num2" "=" "num1 - num2")
+elif choice == "4":
+      if num2 ==0:
+           print("math error")
+      else:
+           print("num1" "+" "num2" "=" "num1 + num2")
